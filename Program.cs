45d44ee289f2098a8e475a93cf447f8c617df503
Luiz Otávio <luizotaviomtbs@gmail.com>
2022-05-29 21:14:14 -0300

    full code (obsolete)

--- Program.cs
+++ Program.cs
@@ -1,35 +1,77 @@
-﻿static void Menu()
+﻿using System;
+using System.IO;
+internal class Program
 {
-    Console.Clear();
-    Console.WriteLine("O que você deseja fazer?");
-    Console.WriteLine("1 - Abrir arquivo.");
-    Console.WriteLine("2 - Criar novo arquivo.");
-    Console.WriteLine("0 - Sair.");
-    short option = short.Parse(Console.ReadLine());
-
-    switch (option)
+    private static void Main(string[] args)
     {
-        case 0:
-            System.Environment.Exit(0);
-            break;
-        case 1:
-            Abrir();
-            break;
-        case 2:
-            Editar();
-            break;
-        default:
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("O que você deseja fazer?");
+            Console.WriteLine("1 - Abrir arquivo.");
+            Console.WriteLine("2 - Criar novo arquivo.");
+            Console.WriteLine("0 - Sair.");
+            short option = short.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 0:
+                    Environment.Exit(0);
+                    break;
+                case 1:
+                    Abrir();
+                    break;
+                case 2:
+                    Editar();
+                    break;
+                default:
+                    Menu();
+                    break;
+            }
+        }
+
+        static void Abrir()
+        {
+            Console.Clear();
+            Console.WriteLine("Qual o caminho do arquivo?");
+            string path = Console.ReadLine();
+            using (var file = new StreamReader(file))
+            {
+                string text = file.ReadToEnd();
+                Console.WriteLine(text);
+            }
+
+            Console.WriteLine(" ");
+            Console.ReadLine();
             Menu();
-            break;
+        }
+
+        static void Editar()
+        {
+            Console.Clear();
+            Console.WriteLine("Digite seu texto abaixo (ESQ para sair)");
+            Console.WriteLine("----------------------------");
+            string text = "";
+
+            do
+            {
+                text += Console.ReadLine();
+                text += Environment.NewLine;
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+            Salvar(text);
+        }
+
+        static void Salvar(string text)
+        {
+            Console.Clear();
+            Console.WriteLine("Qual o caminho para salvar o arquivo?");
+            var path = Console.ReadLine();
+            using (var path = new StreamWriter(path))
+            {
+                file.Write(text);
+            }
+            Console.WriteLine($"Arquivo {path} salvo com sucesso!");
+        }
+        Menu();
     }
-}
-
-static void Abrir()
-{
-
-}
-
-static void Editar()
-{
-
 }
---
+++ README.md
@@ -0,0 +1,2 @@
+### Talvez continuado em um outro momento
+> not working :(
